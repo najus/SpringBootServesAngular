2ee2b82c76c3c614f0ec06ff950619846483137c
Sujan <najussujan@gmail.com>
2017-06-20 21:42:21 -0400

    Dependency update and added static pages

--- src/main/java/com/najus/springboot/controller/MainController.java
+++ src/main/java/com/najus/springboot/controller/MainController.java
@@ -12,6 +12,6 @@ public class MainController {
 
     @RequestMapping(value="/",method = RequestMethod.GET)
     public String homepage(){
-        return "index";
+        return "index.html";
     }
 }
---
+++ src/main/resources/static/js/controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by najus on 6/20/17.
+ */
+app.controller('usersController', function($scope) {
+    $scope.headingTitle = "User List";
+});
+
+app.controller('rolesController', function($scope) {
+    $scope.headingTitle = "Roles List";
+});
